6dc331fc0809805a039272143be5541e48236b6b
Rafael Reyes <reyera2@win.eng.vzwnet.com>
2023-01-04 16:53:12 -0500

    first commit

---
+++ decade.py
@@ -0,0 +1,7 @@
+#  This program ask user How old the are
+
+age = int(input("How old are you?\n "))
+
+decades = age // 10
+years = age % 10
+print("You are " + str(decades) + " decades and " + str(years) + " years old. ")
